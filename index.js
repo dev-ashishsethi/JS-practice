6d6c99a2a3bc7b79eb398252b82826540b09ced1
dev-ashishsethi <me.ashishsethi@gmail.com>
2021-11-02 16:56:35 +0530

    done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("fasfsdfasf");
-var txtarea= document.querySelector("text");
+var txtarea= document.querySelector("#text");
+var btntranslate= document.querySelector(".btn-primary");
+var otput= document.querySelector("#output");
+
+// otput.innerText="afasfdasf";
+// inputTxt=txtarea.value;
+btntranslate.addEventListener("click", translate);
+
+function translate(inputTxt){
+    var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"+"?"+"text="
+    +inputTxt;
+
+    fetch(url).then(response => response.json())
+    .then(json =>{otput.innerText=json.contents.translated;});
+    
+}
