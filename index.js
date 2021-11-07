43a8d3fc595cd7da00363f7ce253ee1ccbadffc8
dev-ashishsethi <me.ashishsethi@gmail.com>
2021-11-07 22:23:06 +0530

    beatified

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-var txtarea= document.querySelector("#text1");
-var btntranslate= document.querySelector(".btn-primary");
-var otput= document.querySelector("#output");
+var txtarea = document.querySelector("#text1");
+var btntranslate = document.querySelector(".btn-primary");
+var otput = document.querySelector("#output");
 
-// otput.innerText="afasfdasf";
 
 btntranslate.addEventListener("click", translate);
 
-function translate(){
-    var url=`https://api.funtranslations.com/translate/minion.json?text=${txtarea.value}`;
-    // console.log(inputTxt);
-    fetch(url).then(response => response.json()).then(json =>{
-        otput.innerText= json.contents.translated
+function translate() {
+    var url = `https://api.funtranslations.com/translate/minion.json?text=${txtarea.value}`;
+
+    fetch(url).then(response => response.json()).then(json => {
+        otput.innerText = json.contents.translated
     }).catch(errorHandler);
-    
+
 }
 
-function errorHandler(error){
-    console.log("error is "+error);
+function errorHandler(error) {
+    console.log("error is " + error);
 }
-
-// https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json
-
-// https://api.funtranslations.com/translate/minion.json
-
-
